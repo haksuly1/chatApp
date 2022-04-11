ac1a4ff2a8aba33ba33d52567efa0b85cac95224
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-11 13:50:39 +0200

    edited CustomsAction permissions

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -1,14 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { TouchableOpacity, View, Text, StyleSheet } from "react-native";
-import * as Permissions from "expo-permissions";
+//import * as Permissions from "expo-permissions";
 import * as ImagePicker from "expo-image-picker";
 import * as Location from "expo-location";
+
 import * as firebase from "firebase";
 import "firebase/firestore";
 
 // component for user actions (the '+' button) on message composer
 export default class CustomActions extends React.Component {
+  
+  // allows to pick and upload an image from the device's library
+  pickImage = async () => {
+    // first ask user for permission to access their media library
+    const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+
+    try {
+      // if permission is granted, let user pick an image from library by calling launchImageLibraryAsync
+      if (status === 'granted') {
+        let result = await ImagePicker.launchImageLibraryAsync({
+          // only images are allowed
+          mediaTypes: ImagePicker.MediaTypeOptions.Images
+        }).catch(err => console.log(err));
+
+        // if the user cancel the process and doesn't pick a file 
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImage(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+  /*
   // choose an image from device's gallery to send
   pickImage = async () => {
     // permission to access the user device's gallery
@@ -29,7 +55,30 @@ export default class CustomActions extends React.Component {
       console.error(error);
     }
   };
+*/
 
+  // allows to take a photo with the device's camera
+  takePhoto = async () => {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA, Permissions.MEDIA_LIBRARY_WRITE_ONLY);
+
+    try {
+      if (status === 'granted') {
+        let result = await ImagePicker.launchCameraAsync({
+          mediaTypes: ImagePicker.MediaTypeOptions.Images
+        }).catch(err => console.log(err));
+
+        if (!result.cancelled) {
+          const imageUrl = await this.uploadImage(result.uri);
+          this.props.onSend({ image: imageUrl });
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+
+  /*
   // Take a photo with the device's camera to send
   takePhoto = async () => {
     // permission to access user camera
@@ -50,7 +99,42 @@ export default class CustomActions extends React.Component {
       console.error(error);
     }
   };
+*/
 
+  // uploads images to firebase as blob
+  uploadImage = async (uri) => {
+    const blob = await new Promise((resolve, reject) => {
+      // create new XMLHttpRequest and set its response type to blob
+      const xhr = new XMLHttpRequest();
+      xhr.onload = function () {
+        resolve(xhr.response);
+      };
+      xhr.onerror = function (e) {
+        console.log(e);
+        reject(new TypeError('Network request failed'));
+      };
+      xhr.responseType = 'blob';
+      // opens the connection to retrieve the URI's data (the image)
+      xhr.open('GET', uri, true);
+      xhr.send(null);
+    });
+
+    const imageNameBefore = uri.split('/');
+    const imageName = imageNameBefore[imageNameBefore.length - 1];
+
+    // create a reference to the storage
+    const ref = firebase.storage().ref().child(`images/${imageName}`);
+    // use put to store the content retrieved from the AJAX request
+    const snapshot = await ref.put(blob);
+
+    // close the connection
+    blob.close();
+
+    return await snapshot.ref.getDownloadURL();
+  }
+
+
+/*
   // Upload an image to Firestore
   uploadImage = async (uri) => {
     const blob = await new Promise((resolve, reject) => {
@@ -78,7 +162,34 @@ export default class CustomActions extends React.Component {
 
     return await snapshot.ref.getDownloadURL();
   };
+  */
 
+  // allows to send the device's current geo-location
+  getLocation = async () => {
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    try {
+      if (status === 'granted') {
+        // getCurrentPositionAsync reads current location data and returns object with coordinates of location
+        let result = await Location.getCurrentPositionAsync({})
+          .catch(err => console.log(err));
+
+        if (result) {
+          // update the state location
+          this.props.onSend({
+            location: {
+              latitude: result.coords.latitude,
+              longitude: result.coords.longitude
+            }
+          });
+        }
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+/*
   // Access and send the user's location
   getLocation = async () => {
     // permission to access user location while the app is in the foreground
@@ -104,7 +215,40 @@ export default class CustomActions extends React.Component {
       console.error(error);
     }
   };
+  */
 
+  // when onActionPress is called, an ActionSheet that displays a set of defined actions is created
+  onActionPress = () => {
+    // strings to be displayed in the ActionSheet
+    const options = [
+      'Choose from Library',
+      'Take Picture',
+      'Send Location',
+      'Cancel'
+    ];
+    // determine position of cancel button in ActionSheet, which closes the view
+    const cancelButtonIndex = options.length - 1;
+    // used to hand down data (the options to display) to the ActionSheet component
+    this.context.actionSheet().showActionSheetWithOptions(
+      {
+        options,
+        cancelButtonIndex
+      },
+      async (buttonIndex) => {
+        switch (buttonIndex) {
+          case 0:
+            return this.pickImage();
+          case 1:
+            return this.takePhoto();
+          case 2:
+            return this.getLocation();
+        }
+      }
+    );
+  }
+
+
+/*
   // Handling all communication features
   onActionPress = () => {
     const options = [
@@ -134,6 +278,7 @@ export default class CustomActions extends React.Component {
       }
     );
   };
+  */
 
   render() {
     return (
@@ -153,6 +298,33 @@ export default class CustomActions extends React.Component {
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    width: 26,
+    height: 26,
+    marginLeft: 10,
+    marginBottom: 10
+  },
+  wrapper: {
+    borderRadius: 13,
+    borderColor: '#b2b2b2',
+    borderWidth: 2,
+    flex: 1
+  },
+  iconText: {
+    color: '#b2b2b2',
+    fontWeight: 'bold',
+    fontSize: 16,
+    backgroundColor: 'transparent',
+    textAlign: 'center'
+  }
+});
+
+CustomActions.contextTypes = {
+  actionSheet: PropTypes.func
+};
+
+/*
 const styles = StyleSheet.create({
   container: {
     width: 26,
@@ -178,3 +350,5 @@ const styles = StyleSheet.create({
 CustomActions.contextTypes = {
   actionSheet: PropTypes.func,
 };
+*/
+
