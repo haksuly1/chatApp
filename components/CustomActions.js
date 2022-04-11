159f09f1ad21a0a67b3e1455bb82b048adaebdaa
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-11 16:48:39 +0200

    troubleshooting

--- components/CustomActions.js
+++ components/CustomActions.js
@@ -161,34 +161,28 @@ export default class CustomActions extends React.Component {
   }
 }
 
+CustomActions.contextTypes = {
+  actionSheet: PropTypes.func,
+};
+
 const styles = StyleSheet.create({
   container: {
     width: 26,
     height: 26,
     marginLeft: 10,
-    marginBottom: 10
+    marginBottom: 10,
   },
   wrapper: {
     borderRadius: 13,
-    borderColor: '#b2b2b2',
+    borderColor: "#b2b2b2",
     borderWidth: 2,
-    flex: 1
+    flex: 1,
   },
   iconText: {
-    color: '#b2b2b2',
-    fontWeight: 'bold',
+    color: "#b2b2b2",
+    fontWeight: "bold",
     fontSize: 16,
-    backgroundColor: 'transparent',
-    textAlign: 'center'
-  }
+    backgroundColor: "transparent",
+    textAlign: "center",
+  },
 });
-
-/**
- * Before you can use this.context, you have to create an object to define this context type.
- * Gifted Chat expects actionSheet to be a function.
- * With PropTypes you can define actionSheet as a function so you can use
- * this.context.actionSheet().showActionSheetWithOptions in your onActionPress function.
- */
-CustomActions.contextTypes = {
-  actionSheet: PropTypes.func
-};
