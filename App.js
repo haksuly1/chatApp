48ff90016e8f76965ee5a4b64d868ae6d6593e36
haksuly1 <93102959+haksuly1@users.noreply.github.com>
2022-04-11 15:50:23 +0200

    troubleshooting to select phtot from gallery

--- App.js
+++ App.js
@@ -1,18 +1,24 @@
 
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import react native gesture handler
-import 'react-native-gesture-handler';
+import "react-native-gesture-handler";
 //import react navigation
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 // import the screens
-import Start from './components/Start';
-import Chat from './components/Chat';
+import Start from "./components/Start";
+import Chat from "./components/Chat";
 
 // create the navigator
 const Stack = createStackNavigator();
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      text: ""
+    }
+  }
   render() {
     return (
       <NavigationContainer>
